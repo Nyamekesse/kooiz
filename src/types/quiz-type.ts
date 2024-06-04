f8bdfa35b627e65ed08be9659173caa4fec7c5e7
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-06-04 13:03:10 +0000

    created a types file to contain all type declarations for app

---
+++ src/types/quiz-type.ts
@@ -0,0 +1,28 @@
+export enum QuizDifficulty {
+  Mixed = '',
+  Easy = 'easy',
+  Medium = 'medium',
+  Hard = 'hard',
+}
+
+export enum QuizType {
+  Mixed = '',
+  Multiple = 'multiple',
+  Boolean = 'boolean',
+}
+
+export interface FetchQuizParams {
+  amount: number;
+  category: string;
+  difficulty: QuizDifficulty;
+  type: QuizType;
+}
+
+export interface QuizCategory {
+  id: number;
+  name: string;
+}
+
+export interface FetchQuizCategoriesResponse {
+  trivia_categories: QuizCategory[];
+}
