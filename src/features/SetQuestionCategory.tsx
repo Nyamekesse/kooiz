05ce27da1b3921c03dfa0cbcf051775eb2d779d6
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-06-04 14:27:08 +0000

    set button to call onClickNext function passed from parent component, displayed the list of categories

--- src/features/SetQuestionCategory.tsx
+++ src/features/SetQuestionCategory.tsx
@@ -1,17 +1,41 @@
-import { useEffect, useState } from 'react';
-import { QuizAPI } from '../api/quiz-api';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+import { Button, Flex, Heading, Radio, RadioGroup, SimpleGrid } from '@chakra-ui/react';
+import { useState } from 'react';
 import { QuizCategory } from '../types/quiz-type';
 
-const SetQuestionCategory = () => {
-  const [categories, setCategories] = useState<QuizCategory[]>([]);
+interface Props {
+  categories: QuizCategory[];
+  onClickNext: (categoryId: string) => void;
+}
 
-  useEffect(() => {
-    (async () => {
-      setCategories(await QuizAPI.fetchCategories());
-    })();
-  }, []);
+const SetQuestionCategory = (p: Props) => {
+  const [selectedCategoryId, setSelectedCategoryId] = useState<string>(p.categories[0].id.toString());
 
-  return <div>SetQuestionCategory</div>;
+  const radioList = p.categories.map((category: QuizCategory) => {
+    return (
+      <Radio key={category.id} value={category.id.toString()}>
+        {category.name}
+      </Radio>
+    );
+  });
+
+  return (
+    <>
+      <Flex direction={'column'} alignItems={'center'}>
+        <Heading as={'h1'} fontSize={'3xl'} mb={18}>
+          Which Topic?
+        </Heading>
+      </Flex>
+      <RadioGroup display={'flex'} justifyContent={'center'} value={selectedCategoryId} onChange={setSelectedCategoryId}>
+        <SimpleGrid columns={[1, 3, 4]} spacing={4}>
+          {radioList}
+        </SimpleGrid>
+      </RadioGroup>
+      <Button onClick={() => p.onClickNext(selectedCategoryId)} position={'absolute'} top={'90%'} right={'10%'} rightIcon={<ArrowForwardIcon />}>
+        Set Difficulty
+      </Button>
+    </>
+  );
 };
 
 export default SetQuestionCategory;
