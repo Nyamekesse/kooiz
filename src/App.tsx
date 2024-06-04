1cdb05fab1fc740a555cc558cc611fd16edd7151
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-06-04 12:01:29 +0000

    added the component to render screen based on state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+import { Box, Flex, Image } from '@chakra-ui/react';
+import { useState } from 'react';
+import bubbleImg from './assets/bubble.png';
+import logoImg from './assets/logo.png';
+import SetQuestionQty from './features/SetQuestionQty';
 
-function App() {
-
-  return (
-    <>
-     <h1>Hi</h1>
-    </>
-  )
+enum Step {
+  SetQuestionQty,
+  SetQuestionCategory,
+  SetQuestionDifficulty,
+  Play,
+  Score,
 }
 
-export default App
+function App() {
+  const [step, setStep] = useState<Step>(Step.SetQuestionQty);
+  const header = (
+    <Flex justify={'center'}>
+      <Image h={24} src={logoImg} />
+    </Flex>
+  );
+  const renderScreenByStep = () => {
+    switch (step) {
+      case Step.SetQuestionQty:
+        return <SetQuestionQty max={30} min={5} step={5} defaultValue={10} />;
+      case Step.SetQuestionCategory:
+        return <></>;
+      case Step.SetQuestionDifficulty:
+        return <></>;
+      case Step.Play:
+        return <></>;
+      case Step.Score:
+        return <></>;
+      default:
+        return null;
+    }
+  };
+  return (
+    <Box py={10} h={'100%'} position={'relative'}>
+      {header}
+      <Image src={bubbleImg} position={'absolute'} zIndex={-1} right={120} top={100} />
+      <Box mt={100}>{renderScreenByStep()}</Box>
+    </Box>
+  );
+}
+
+export default App;
