bb5ef10c1aceec383e4bcf619e0f10e362d4eae5
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-06-05 12:18:36 +0000

    added component to display fetch questions

---
+++ src/features/PlayQuiz.tsx
@@ -0,0 +1,11 @@
+import { QuizItem } from '../types/quiz-type';
+
+interface Props {
+  quiz: QuizItem[];
+}
+
+const PlayQuiz = (p: Props) => {
+  return <div>PlayQuiz</div>;
+};
+
+export default PlayQuiz;
