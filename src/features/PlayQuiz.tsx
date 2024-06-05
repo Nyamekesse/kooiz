f84e1357f238095ab63217a2dbeb389098d63c69
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-06-05 13:33:17 +0000

    shuffled the answers for each question to make sure the correct answer is not always the first option and also added coloring to the right and wrong answers after a user selecteds an option

--- src/features/PlayQuiz.tsx
+++ src/features/PlayQuiz.tsx
@@ -13,9 +13,9 @@ interface Props {
 const PlayQuiz = (p: Props) => {
   const [answer, setAnswer] = useState<string>('');
   const [questionStatus, setQuestionStatus] = useState<'valid' | 'invalid' | 'unanswered'>('unanswered');
+  const [availableAnswers, setAvailableAnswers] = useState<string[]>([]);
   const [currentQuizItemIndex, setCurrentQuizItemIndex] = useState<number>(0);
   const currentQuizItem: QuizItem = p.quiz[currentQuizItemIndex];
-  const availableAnswers: string[] = [currentQuizItem.correct_answer, ...currentQuizItem.incorrect_answers];
 
   const isValidAnswer = (answer: string): boolean => {
     return answer === currentQuizItem.correct_answer;
@@ -31,17 +31,24 @@ const PlayQuiz = (p: Props) => {
     }
   }, [answer]);
 
+  useEffect(() => {
+    setAvailableAnswers([currentQuizItem.correct_answer, ...currentQuizItem.incorrect_answers].sort(() => Math.random() - 0.5));
+  }, [currentQuizItemIndex]);
+
   const radioList = availableAnswers.map((availableAnswer: string) => {
     return (
       <Radio key={availableAnswer} value={availableAnswer}>
-        <Text dangerouslySetInnerHTML={{ __html: currentQuizItem.question }} />
+        <Text
+          color={questionStatus === 'unanswered' ? 'black' : isValidAnswer(availableAnswer) ? 'green.400' : 'red.400'}
+          dangerouslySetInnerHTML={{ __html: currentQuizItem.question }}
+        />
       </Radio>
     );
   });
   return (
     <Flex direction={'column'} alignItems={'center'} justify={'center'}>
       <Heading fontSize={'3xl'} mt={5} mb={5} dangerouslySetInnerHTML={{ __html: currentQuizItem.question }} />
-      <RadioGroup value={answer} onChange={setAnswer}>
+      <RadioGroup value={answer} onChange={questionStatus === 'unanswered' ? setAnswer : undefined}>
         <SimpleGrid columns={2} spacing={4}>
           {radioList}
         </SimpleGrid>
