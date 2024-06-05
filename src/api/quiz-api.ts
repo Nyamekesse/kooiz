46228b1bab6560e1f776d1247465da3ffae86c9a
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-06-05 12:20:18 +0000

    created the fetchQuiz function to fetch quiz questions using the parameters

--- src/api/quiz-api.ts
+++ src/api/quiz-api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FetchQuizCategoriesResponse, QuizCategory } from '../types/quiz-type';
+import { FetchQuizCategoriesResponse, FetchQuizParams, FetchQuizResponse, QuizCategory, QuizItem } from '../types/quiz-type';
 
 const BASE_URL = 'https://opentdb.com/';
 
@@ -7,5 +7,9 @@ export class QuizAPI {
   static async fetchCategories(): Promise<QuizCategory[]> {
     const { data } = await axios.get<FetchQuizCategoriesResponse>(`${BASE_URL}/api_category.php`);
     return data.trivia_categories;
+  }
+  static async fetchQuiz(params: FetchQuizParams): Promise<QuizItem[]> {
+    const { data } = await axios.get<FetchQuizResponse>(`${BASE_URL}/api.php`, { params });
+    return data.results;
   }
 }
